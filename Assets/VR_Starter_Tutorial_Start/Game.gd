d2ab83fb8bcd8d11bda4334ec5f11c14386cbd3a
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-05-03 21:55:26 -0600

    Código añadido a Game.gd y VR_Controller.gd

--- Assets/VR_Starter_Tutorial_Start/Game.gd
+++ Assets/VR_Starter_Tutorial_Start/Game.gd
@@ -1,11 +1,15 @@
-extends Node3D
+extends Spatial  
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var VR = ARVRServer.find_interface("OpenVR")
+	if VR and VR.initialize():
+		get_viewport().arvr = true
+		get_viewport().hdr = false
+
+		OS.vsync_enabled = false
+		Engine.target_fps = 90
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	#pass
