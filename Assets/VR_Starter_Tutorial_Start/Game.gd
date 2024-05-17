35109c8ce88050044402624b5ee8e158b3e4541e
natmsaenz <natalia.madrigal2941@alumnos.udg.mx>
2024-05-17 15:12:00 -0600

    Código y funciones añadidas a Game.gd

--- Assets/VR_Starter_Tutorial_Start/Game.gd
+++ Assets/VR_Starter_Tutorial_Start/Game.gd
@@ -8,7 +8,14 @@ func _ready():
 
 		OS.vsync_enabled = false
 		Engine.target_fps = 90
+	var spheres_left = 10
+	var sphere_ui = null
 
+func remove_sphere():
+	spheres_left -= 1
+
+	if sphere_ui:
+		sphere_ui.update_ui(spheres_left)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
